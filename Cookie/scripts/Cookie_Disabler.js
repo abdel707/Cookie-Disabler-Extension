effc84db874f2241c6cc295811624d5d9559dfd4
Abdel707 <abdelrahni.ett@gmail.com>
2025-10-06 06:54:30 +0200

    Adding aria-label management

--- Cookie/scripts/Cookie_Disabler.js
+++ Cookie/scripts/Cookie_Disabler.js
@@ -59,12 +59,14 @@
     }
 
     function clickRejectButton(){
-        const rejectButton = Array.from(document.querySelectorAll('button, input[type="button"], a, aria-label')).find(element =>
+        const rejectButton = Array.from(document.querySelectorAll('button, input[type="button"], a, button[aria-label]')).find(element =>
         (element.textContent || '').toLowerCase().includes('reject')||
         (element.textContent || '').toLowerCase().includes('refuser')||
+        (element.textContent || '').toLowerCase().includes('decline')||
+        (element.textContent || '').toLowerCase().includes("do not consent")||
         (element.textContent || '').toLowerCase().includes('optionnels')||
-        (element.textContent || '').toLowerCase().includes('Continuer sans accepter')||
-        (element.textContent || '').toLowerCase().includes('Refuser les cookies optionnels')
+        (element.textContent || '').toLowerCase().includes("continuer sans accepter")||
+        (element.textContent || '').toLowerCase().includes("refuser les cookies optionnels")
         );
         if(rejectButton){
             rejectButton.click();
